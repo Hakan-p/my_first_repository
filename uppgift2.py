37ebd378e2288ba7836693b49d73806ca11a844e
Håkan Nilsson <hakannilsson@Hakans-MacBook-Air.local>
2025-01-11 08:56:10 +0100

    First revision

---
+++ uppgift2.py
@@ -0,0 +1,6 @@
+# Programmet beräknar kassa - utgift samt vad hälften av kassan som är kvar
+utgift = 100  # biljettpris
+kassa = 200  # pengar på fickan
+print ("Det blir  ", kassa - utgift , " kronor över.") 
+halvakassan = (kassa - utgift) / 2
+print("Hälften är: ", halvakassan  )  
