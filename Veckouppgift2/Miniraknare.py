b05319ddd4f5c0789f6b683c2d8aa87cb0b674ee
Håkan Nilsson <hakannilsson@Hakans-MacBook-Air.local>
2025-01-16 00:30:02 +0100

    justerad

--- Veckouppgift2/Miniraknare.py
+++ Veckouppgift2/Miniraknare.py
@@ -1,9 +1,11 @@
 # Miniräknare 
+# Summera tal1 och tal2 och tal3
 tal1 = float(input("Mata in ett tal "))
 tal2 = float(input("Mata in ett tal "))
 tal3 = float(input("Mata in ett tal "))
 summera = tal1+tal2+tal3
 print(f"Summan av talen är", {summera})
+# Vilket tal är störst
 tal1_great = tal1 > tal2 > tal3
 tal2_great = tal2 > tal1 and tal2 > tal3
 tal3_great = tal3 > tal1 and tal3 > tal2
@@ -14,19 +16,20 @@ elif tal2_great == True :
     print("tal2 är störst")
 elif tal3_great == True :
     print("tal3 är störst")    
-
-tal1_mittersta = tal2 < tal1 and tal1 < tal3
-tal2_mittersta = tal1 < tal2 and tal3 > tal2
-tal3_mittersta = tal1 < tal3 and tal2 > tal3  or tal1 > tal2 and tal3 > tal2
+# Vilket Tal är Lika
+# Vilket Tal är mitterst
+box1_mittersta = tal1 > tal2 and tal2 < tal3 and tal1 < tal3  or tal1 < tal2 and tal2 > tal3 and tal1 > tal3
+box2_mittersta = tal1 < tal2 and tal2 < tal3 and tal1 < tal3 or tal1 > tal2 and tal2 > tal3 and tal1 > tal3
+box3_mittersta = tal1 < tal2 and tal2 > tal3 and tal1 < tal3  or tal1 > tal2 and tal2 < tal3 and tal1 > tal3
 tal1_lika_tal2_lika_tal3 = tal1 == tal2 and tal1 == tal3
 tal1_lika_tal2 = tal1 == tal2
 tal2_lika_tal3 = tal2 == tal3 
 tal3_lika_tal1 = tal3 == tal1
-if tal1_mittersta:
+if box1_mittersta:
     print("Tal1 är det mittersta") 
-elif tal2_mittersta:
+elif box2_mittersta:
     print("Tal2 är det mittersta")
-elif tal3_mittersta:
+elif box3_mittersta:
     print("Tal3 är det mittersta")
 elif tal1_lika_tal2_lika_tal3 :
     print("Alla tal är lika")
