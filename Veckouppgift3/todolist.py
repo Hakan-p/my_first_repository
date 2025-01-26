c8539fc9131b2ba1eafe667b4b7b3ef61749420f
Håkan Nilsson <hakannilsson@Hakans-MacBook-Air.local>
2025-01-26 23:14:05 +0100

    nu väljer vi operativsystem

--- Veckouppgift3/todolist.py
+++ Veckouppgift3/todolist.py
@@ -13,7 +13,16 @@ def huvudmeny():
     print("*******************************************")
     print("*******************************************") 
 import os
-os.system('clear')
+os_type = os.name  # Kan vara 'posix', 'nt', eller 'os2', beroende på operativsystemet
+if os_type == 'posix':
+    #("Detta är ett Unix-liknande OS (t.ex. Linux eller macOS)")
+    ostyp = "clear" 
+elif os_type == 'nt':
+    #("Detta är Windows")
+    ostyp ="cls"
+else:
+    print("Okänt operativsystem")
+os.system(ostyp)
 lista =[]
 lista_borttagna=[]
 list_item =""
@@ -23,21 +32,21 @@ huvudmeny()
 while alt !="5":
     alt = input ("Välj ett alternativ :") 
     if alt == ("1"):
-        os.system('clear')
+        os.system(ostyp)
         print("Du har valt att se innhehållet i din listal")
         print(lista)
         input("Tryck på Enter för att fortsätta...")
         os.system('clear')
         huvudmeny()
     elif alt == ("2"):
-        os.system('clear')
+        os.system(ostyp)
         print("Du har valt att lägga till innhehållet i din listal")
         list_item =input("Varsågod och börja nu kan du lägga in i din lista :")
         lista.append(list_item)
-        os.system('clear')
+        os.system(ostyp)
         huvudmeny()
     elif alt ==("3"):
-        os.system('clear')
+        os.system(ostyp)
         print(lista)        
         list_item_bort = input("Vilken grej är du färdig med ?")
         lista.remove(list_item_bort)
@@ -50,10 +59,17 @@ while alt !="5":
         list_item_ater = input("Vilken grej vill du lägga tillbaka i listan ?")
         lista_borttagna.remove(list_item_ater)
         lista.append(list_item_ater)
-        os.system('clear')
+        os.system(ostyp)
         huvudmeny()
     else:
-        os.system('clear')
-        print("Du har valt att avsluta")   
+        os.system(ostyp)
+        if alt !="5":
+            print("Du har gjort ett felaktigt alternativ !!")
+            input("Tryck på Enter för att fortsätta...")
+            os.system(ostyp)
+            huvudmeny()
+        else:      
+            alt ="5"
+            print("Du har valt att avsluta")   
                    
 print(f"listan innehåller nedanstående element {lista}")
---
+++ Veckouppgift3/x=0.py
@@ -0,0 +1,11 @@
+x = 0
+y = 1
+while y < 10: 
+    if y % 2 ==0:
+        x -= y
+    else:
+        x += y * y    
+    y =+ 1
+ print(y)
+   print(x)
+  
